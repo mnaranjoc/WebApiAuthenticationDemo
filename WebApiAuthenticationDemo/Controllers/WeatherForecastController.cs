fcb30190ee951f274a021b03f7797223e99e8763
Manuel Naranjo Contreras <manuelnaranjo@outlook.com>
2021-07-02 16:42:20 -0500

    Project setup

---
+++ WebApiAuthenticationDemo/Controllers/WeatherForecastController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiAuthenticationDemo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> NoAuth()
+        {
+            return GetWeather();
+        }
+
+        [HttpGet]
+        [Route("auth/basic")]
+        public IEnumerable<WeatherForecast> BasicAuth()
+        {
+            return GetWeather();
+        }
+
+        [HttpGet]
+        [Route("auth/apikey")]
+        public IEnumerable<WeatherForecast> ApiKey()
+        {
+            return GetWeather();
+        }
+
+        [HttpGet]
+        [Route("auth/bearertoken")]
+        public IEnumerable<WeatherForecast> BearerToken()
+        {
+            return GetWeather();
+        }
+
+        private IEnumerable<WeatherForecast> GetWeather()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
